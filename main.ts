98d0a58f4133fefd6c745dd1e7737b4c5b2758fe
shivraj18 <71889717+shivraj18@users.noreply.github.com>
2020-09-26 00:16:49 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -73,11 +73,11 @@ let frontCam = sprites.create(img`
 frontCam.setPosition(60, 60)
 frontCam.setVelocity(speed, 0)
 let Goose = sprites.create(img`
-    . . . . . . f f f f f . . . . . 
-    . . . . . f f 1 1 1 1 f f . . . 
-    . . . . . f 1 1 f 1 1 1 f . . . 
-    . . . . . f 1 1 1 1 2 2 f f . . 
-    . . . . . f 1 1 f 1 2 2 2 f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f f f . . . 
+    . . . . . f 1 1 1 1 1 1 f . . . 
+    . . . . . f 1 1 f 1 2 2 f f . . 
+    . . . . . f 1 1 1 1 2 2 2 f . . 
     . . . . . f 1 1 f f f f f . . . 
     . . . . . f 1 1 f . . . . . . . 
     . . . . . f 1 1 f . . . . . . . 
@@ -127,11 +127,11 @@ let enemyList = [sprites.create(img`
     . . f f f f f f f f f f f f . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Enemy), sprites.create(img`
-    . . . . . . f f f f f . . . . . 
-    . . . . . f f 1 1 1 1 f f . . . 
-    . . . . . f 1 1 f 1 1 1 f . . . 
-    . . . . . f 1 1 1 1 2 2 f f . . 
-    . . . . . f 1 1 f 1 2 2 2 f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f f f . . . 
+    . . . . . f 1 1 1 1 1 1 f . . . 
+    . . . . . f 1 1 f 1 2 2 f f . . 
+    . . . . . f 1 1 1 1 2 2 2 f . . 
     . . . . . f 1 1 f f f f f . . . 
     . . . . . f 1 1 f . . . . . . . 
     . . . . . f 1 1 f . . . . . . . 
@@ -144,11 +144,11 @@ let enemyList = [sprites.create(img`
     . . . . f 2 f . . f 2 f . . . . 
     . . . . f f f . . f f f . . . . 
     `, SpriteKind.Enemy), sprites.create(img`
-    . . . . . . f f f f f . . . . . 
-    . . . . . f f 1 1 1 1 f f . . . 
-    . . . . . f 1 1 f 1 1 1 f . . . 
-    . . . . . f 1 1 1 1 2 2 f f . . 
-    . . . . . f 1 1 f 1 2 2 2 f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f f f . . . 
+    . . . . . f 1 1 1 1 1 1 f . . . 
+    . . . . . f 1 1 f 1 2 2 f f . . 
+    . . . . . f 1 1 1 1 2 2 2 f . . 
     . . . . . f 1 1 f f f f f . . . 
     . . . . . f 1 1 f . . . . . . . 
     . . . . . f 1 1 f . . . . . . . 
