d8d35e71e1e3b8507783ea66616f1cb6cd93d3ae
Atalsot <attila.the.hun.zsoldos@gmail.com>
2020-09-25 13:26:05 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,11 +7,6 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         lane = 1
     }
 })
-function add_new_obstacle_not_functional () {
-    newEnemy = enemyList[randint(0, 2)]
-    newEnemy.setPosition(character.x + (120 + randint(0, maxObstacleDistance)), randint(1, 3) * 30)
-    console.log(enemyList.length)
-}
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     lane += 1
     if (lane > 3) {
@@ -21,15 +16,11 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
 	
 })
-let newEnemy: Sprite = null
-let enemyList: Sprite[] = []
-let character: Sprite = null
-let maxObstacleDistance = 0
 let lane = 0
 let speed = 96
 lane = 2
-maxObstacleDistance = 280
-character = sprites.create(img`
+let maxObstacleDistance = 280
+let character = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -71,7 +62,7 @@ frontCam.setPosition(60, 60)
 frontCam.setVelocity(speed, 0)
 scene.cameraFollowSprite(frontCam)
 tiles.setTilemap(tilemap`level_1`)
-enemyList = [sprites.create(img`
+let enemyList = [sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -143,7 +134,24 @@ enemyList = [sprites.create(img`
 for (let value of enemyList) {
     value.setPosition(0, 200)
 }
-let pointList = sprites.allOfKind(SpriteKind.Food)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 5 5 5 5 . . . . . . . 
+    . . . . 5 . . . . 5 . . . . . . 
+    . . . . 5 . 5 5 . 5 . . . . . . 
+    . . . . 5 . 5 5 . 5 . . . . . . 
+    . . . . 5 . . . . 5 . . . . . . 
+    . . . . . 5 5 5 5 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
 game.onUpdateInterval(1000, function () {
     if (character.x >= 1000) {
         character.setPosition(50, 30 * lane)
